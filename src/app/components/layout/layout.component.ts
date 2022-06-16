12db26dc5080ca98a3490a9d4cbf1d9651310d1b
juan pablo <jpagudelo2000@gmal.com>
2022-06-16 14:23:27 -0500

    sidebar styles

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, Subscription } from 'rxjs';
+
+import { StoreService } from '@services/store/store.service';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent implements OnInit, OnDestroy {
+  isProfile: boolean = true;
 
-  constructor() { }
+  subscriber: Subscription | null = null;
 
-  ngOnInit(): void {
+  constructor(
+    private location: Location,
+    private router: Router,
+    private storeService: StoreService
+  ) {
+    this.isProfile = this.location.path().includes('profile');
   }
 
+  ngOnInit(): void {
+    this.subscriber = this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        this.isProfile = this.location.path().includes('profile');
+      });
+  }
+
+  toggleSidebar() {
+    this.storeService.toggleSidebar();
+  }
+
+  ngOnDestroy() {
+    this.subscriber?.unsubscribe();
+  }
 }
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { StoreService } from '@services/store/store.service';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  activeSidebar: boolean = false;
 
-  constructor() { }
+  constructor(private storeService: StoreService) {}
 
   ngOnInit(): void {
+    this.storeService.activeSidebar$.subscribe((value) => {
+      this.activeSidebar = value;
+    });
   }
 
+  toggleSidebar() {
+    this.storeService.toggleSidebar();
+  }
 }
